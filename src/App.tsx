05c7ba2727421c09477b170e85644552253a0e30
Yusril Ahmad <yusril7680@gmail.com>
2023-10-12 14:34:02 +0700

    meme-generator

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MemeGenerator from './MemeGenerator';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>MEME GENERATOR</h1>
+      <div className="card">
+        <MemeGenerator />
+      </div>
+    </>
+  );
+}
+
+export default App;
